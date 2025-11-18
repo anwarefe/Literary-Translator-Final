fbb5e6a48c9e63bc0dd92b0ae9f3264112cdc00e
anwarefe <anwarefe@gmail.com>
2025-11-18 04:30:31 +0200

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -43,3 +43,17 @@ export const MicrosoftIcon: React.FC<IconProps> = (props) => (
     <rect x="13" y="13" width="7" height="7" />
   </svg>
 );
+
+export const UploadIcon: React.FC<IconProps> = (props) => (
+  <svg
+    {...props}
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    {/* سهم لأعلى */}
+    <path d="M12 3l-4 4h3v5h2V7h3z" />
+    {/* خط تحت */}
+    <rect x="5" y="17" width="14" height="2" />
+  </svg>
+);
